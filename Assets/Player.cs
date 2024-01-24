4a52ec15dc4eaeb8b521b75ac823276a8184cf53
Joe Hill <91340791+Subluxx@users.noreply.github.com>
2024-01-24 17:27:06 +0000

    animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,27 +6,49 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
+    Animator anim;
 
     Vector2 movement;
     public Transform SpawnPos;
 
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     void Update()
     {
-        if (gameObject.CompareTag("Player1"))
+
+        anim.SetFloat("xspeed", rb.velocity.x);
+        anim.SetFloat("yspeed", rb.velocity.y);
+        if (rb.velocity.magnitude < 0.01)
         {
-            movement.x = Input.GetAxisRaw("Horizontal");
-            movement.y = Input.GetAxisRaw("Vertical");
+           // anim.speed = 0.0f;
         }
-        else if (gameObject.CompareTag("Player2"))
+        else
         {
-            movement.x = Input.GetAxisRaw("Horizontal2");
-            movement.y = Input.GetAxisRaw("Vertical2");
+            anim.speed = 1.0f;
         }
+        Debug.Log(rb.velocity);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (gameObject.CompareTag("Player1"))
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+            if (rb.velocity.magnitude > moveSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+            }
+        }
+        else if (gameObject.CompareTag("Player2"))
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2")) * moveSpeed;
+            if (rb.velocity.magnitude > moveSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
