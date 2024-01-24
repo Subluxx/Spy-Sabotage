30cd53fbc7d6afcea8354e6c8d23fcd7aa1e13c6
Joe Hill <josephphill@me.com>
2024-01-24 01:25:23 +0000

    map changes and camera fix

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -6,7 +6,7 @@ public class CameraScript : MonoBehaviour
     public Transform assignedPlayer;
     private void LateUpdate()
     {
-        Vector3 newPos = new Vector3(assignedPlayer.position.x, transform.position.y, -10);
+        Vector3 newPos = new Vector3(assignedPlayer.position.x, assignedPlayer.position.y, -10);
         transform.position = newPos;
     }
 }
