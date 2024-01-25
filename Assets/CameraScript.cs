fbc6c70aff951b552d079b8855b4784430fd8372
Joe Hill <91340791+Subluxx@users.noreply.github.com>
2024-01-25 13:15:15 +0000

    player anim 2 and intro cam start

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -4,9 +4,37 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public Transform assignedPlayer;
+    bool reachedPlayer = false;
+    float cameraSpeed = 1f;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+      
+    }
     private void LateUpdate()
     {
-        Vector3 newPos = new Vector3(assignedPlayer.position.x, assignedPlayer.position.y, -10);
-        transform.position = newPos;
+        if (reachedPlayer) 
+        {
+            Vector3 newPos = new Vector3(assignedPlayer.position.x, assignedPlayer.position.y, -10);
+            transform.position = newPos;
+        }
+        else if (!reachedPlayer)
+        {
+            float interpolation = cameraSpeed * Time.deltaTime;
+
+            Vector3 position = this.transform.position;
+            position.y = Mathf.Lerp(this.transform.position.y, assignedPlayer.transform.position.y, interpolation);
+            position.x = Mathf.Lerp(this.transform.position.x, assignedPlayer.transform.position.x, interpolation);
+
+            this.transform.position = position;
+
+            if (position == assignedPlayer.transform.position)
+            {
+                reachedPlayer = true;
+            }
+        }
     }
 }
---
+++ Assets/StartCamScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCamScript : MonoBehaviour
+{
+    public RectTransform SplitScreenBoarder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
