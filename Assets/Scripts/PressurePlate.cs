6ae73af78bb795db5a8192fa9b58cb94ba41ff8b
jackf21 <jack.fenning04@gmail.com>
2024-01-25 14:56:37 +0000

    Pressure Plate Animation

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -7,13 +7,38 @@ using UnityEngine.WSA;
 public class PressurePlate : MonoBehaviour
 {
     [SerializeField] List<GameObject> traps = new List<GameObject>();  //Array of Traps
+    [SerializeField] Sprite pressurePlateOff;
+    [SerializeField] Sprite pressurePlateOn;
+    [SerializeField] SpriteRenderer pressurePlateRenderer;
+    private bool collided = false;
     public float activeTime;
+    private Timer timer;
+
+    private void Start()
+    {
+        timer = new Timer(activeTime + 1);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        collided = true;
         foreach (GameObject trap in traps) //Make each trap active for a set amount of time
         {
             trap.GetComponent<SpikeTrap>().playAnimation(activeTime);
+        }
+    }
+
+    private void Update()
+    {
+        if(collided)
+        {
+            pressurePlateRenderer.sprite = pressurePlateOn;
+            timer.IncrementTimer();
+            if (timer.CheckAndResetTimer())
+            {
+                pressurePlateRenderer.sprite = pressurePlateOff;
+                collided=false;
+            }
         }
     }
 }
